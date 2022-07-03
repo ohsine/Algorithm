6600f5a2422de7c47aab778382e2fb218e20618d
ohsine <ohsine@korea.ac.kr>
2022-07-03 22:00:36 +0900

    기출_그리디_모험가길드.cpp
    2022.07.30

---
+++ 2022.07.03/기출_그리디_모험가길드.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+
+	int N;
+
+	vector<int> p;
+
+	for (int i = 0; i < 5; i++)
+	{
+		int c;
+		cin >> c;
+		p.push_back(c);
+	}
+
+	sort(p.begin(), p.end());
+
+	int result = 0;
+	int count = 0;
+
+	for (int i = 0; i < p.size(); i++)
+	{
+		count++;
+
+		if (count >= i)
+		{
+			result += 1;
+			count = 0;
+		}
+	}
+
+	cout << result;
+}
